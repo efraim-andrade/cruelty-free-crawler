0ddcd76b98a0c55988ada2b0ee175a80c723d23d
Efraim Andrade <efraim.dev@gmail.com>
2021-05-14 21:28:58 -0300

    finish the crawler with the json list

--- src/index.js
+++ src/index.js
@@ -1,28 +1,58 @@
 const puppeteer = require("puppeteer");
 
-// const generateJsonFile = require("./functions/generateJsonFile");
+const generateJsonFile = require("./functions/generateJsonFile");
 
 const BASE_URL = "http://www.pea.org.br/empresas.htm";
+
+const HTML_TAG_REGEX = /(<([^>]+)>)/gi;
+const UNNECESSARY_WHITESPACE = /\s\s+/g;
+const ALL_BEFORE_TWO_DOTS = /^[^:\r\n]+:\h*/gi;
 
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(BASE_URL);
 
-  const commandsList = await page.evaluate(() => {
-    const nodeList = document.querySelectorAll(".MsoNormal");
+  const brandList = await page.evaluate(() => {
+    const amountOfHTMLCrapWithBrands = document.querySelectorAll("font");
 
-    const listOfBruteBrands = [...nodeList];
+    const listOfBruteBrands = [...amountOfHTMLCrapWithBrands];
 
-    console.log("🚀 ~ commandsList ~ listOfBruteBrands", listOfBruteBrands);
+    let listOfGroupedBrands = [];
 
-    return listOfBruteBrands;
+    const brands = listOfBruteBrands
+      .map((brand) => brand.innerHTML)
+      .filter((brand) => brand.includes("Marcas:"))
+      .map((brand) =>
+        brand
+          .replace(UNNECESSARY_WHITESPACE, "")
+          .replace(HTML_TAG_REGEX, "")
+          .replace(ALL_BEFORE_TWO_DOTS, "")
+          .replace(/Marcas:/g, "")
+          .replace(/&nbsp/g, "")
+      )
+      .filter((brand) => !!brand);
+
+    brands.forEach((brand) => {
+      brand.split(",").forEach((byComma) => {
+        byComma.split(":").forEach((byComma) => {
+          listOfGroupedBrands.push(byComma.split(";"));
+        });
+      });
+    });
+
+    const finalBrandList = listOfGroupedBrands
+      .flat()
+      .map((brand) => brand.trim())
+      .filter((brand) => !!brand);
+
+    return finalBrandList;
   });
 
-  // generateJsonFile({
-  //   fileName: "commands-list",
-  //   fileContent: commandListWithInfo,
-  // });
+  generateJsonFile({
+    fileName: "cruelty-free-brand-list",
+    fileContent: brandList,
+  });
 
   await browser.close();
 })();
